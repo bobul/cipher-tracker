132529abf1b02b318082af98334ed7d4cf36dc13
Ihor Bobulych <ihbobuacc@gmail.com>
2023-11-12 19:52:05 +0200

    Welcome Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="p-8">
+      <h1 className="font-bold text-teal-400 text-3xl">
+        Welcome to Cipher App!
+      </h1>
+      <p className="text-xl text-gray-400">
+        This application allows you to keep track of the encrypted text.
+      </p>
+      <p className="text-lg font-bold text-gray-400">
+        Awesome! 😉
+        <Link href="/encrypt" className="text-teal-400 underline font-normal">
+          Let's try it now!
+        </Link>
+      </p>
+    </div>
   )
 }
